cfdacd8b5ea9ca9835b139d057af122aa6bbbe1d
Mohammadreza Hendiani <mohammad.r.hendiani@gmail.com>
2023-12-22 14:35:02 +0330

    my answers

---
+++ 1/4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cin >> x;
+	switch (x)
+	{
+	case 0:
+		cout << "Rahat mibari\n";
+		break;
+	case 1:
+		cout << "Mibazi\n";
+		break;
+	case 2:
+		cout << "Be sakhti mibari\n";
+		break;
+	case 3:
+		cout << "Shekaste sangin mikhori\n";
+		break;
+	case 4:
+		cout << "Be sakhti mibari\n";
+		break;
+	case 5:
+		cout << "Mibazi\n";
+		break;
+	case 6:
+		cout << "Rahat mibari\n";
+		break;
+	case 7:
+		cout << "Mibazi\n";
+		break;
+	case 8:
+		cout << "Be sakhti mibari\n";
+		break;
+	case 9:
+		cout << "Shekaste sangin mikhori\n";
+		break;
+	default:
+		cout << "In harif vojud nadarad\n";
+
+	}
+	return 0;
+}
