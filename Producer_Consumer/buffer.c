d885d3a3dc6e9cc9292c93ad9dcd91e9fa972753
Mohammadreza Hendiani <mohammad.r.hendiani@gmail.com>
2023-12-28 01:38:25 +0330

    completed readme

---
+++ Producer_Consumer/buffer.c
@@ -0,0 +1,46 @@
+#include "difinitions.h"
+
+#define buffer_size 4
+
+unsigned int front,rear;
+
+data buffer[buffer_size];
+
+pthread_mutex_t buffer_mutex=PTHREAD_MUTEX_INITIALIZER;
+
+//----------------------------------
+void buffer_init(){
+	front=0;
+	rear=0;
+	pthread_mutex_init(&buffer_mutex, NULL);
+}
+//----------------------------------
+void buffer_destroy(){
+	pthread_mutex_destroy(&buffer_mutex);
+}
+//----------------------------------
+BOOL buffer_push(data *d, unsigned int dsize){
+	pthread_mutex_lock(&buffer_mutex);
+	if ((rear+1)%buffer_size == front){//Buffer is full
+		pthread_mutex_unlock(&buffer_mutex);
+		return FALSE;
+	}
+	memcpy(&buffer[rear], d, dsize);
+	rear=(rear+1)%buffer_size;
+	pthread_mutex_unlock(&buffer_mutex);
+	return TRUE;
+}
+//----------------------------------
+data * buffer_pop(){
+	data *d;
+	pthread_mutex_lock(&buffer_mutex);
+	if (front==rear){//Buffer is empty
+		pthread_mutex_unlock(&buffer_mutex);
+		return NULL;
+	}
+	d=&buffer[front];
+	front=(front+1)%buffer_size;
+	pthread_mutex_unlock(&buffer_mutex);
+	return d;
+}
+//----------------------------------
---
+++ README.md
@@ -0,0 +1,7 @@
+# Embedded & Real-time Systems
+
+`JAVA` projects done with `eclipse`
+
+programming the `ATmega238p` Microcontroller in `C` for `Proteus`
+
+![images](img/a.png)
